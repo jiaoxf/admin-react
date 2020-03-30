732aa67525686f4dfdd5434a9b8c696ba7738743
焦学峰 <13261899915@163.com>
2020-03-30 11:12:03 +0800

    init-1

---
+++ build/precache-manifest.953e03184cf16aab3a0b2d89b53b4332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63396b6c5d10be9af3e63035799f4184",
+    "url": "/index.html"
+  },
+  {
+    "revision": "119f071e0d87dbc657a4",
+    "url": "/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "a1d699064501a5d15306",
+    "url": "/static/css/main.55bd9129.chunk.css"
+  },
+  {
+    "revision": "119f071e0d87dbc657a4",
+    "url": "/static/js/2.79fc9184.chunk.js"
+  },
+  {
+    "revision": "7e941fe2d293014f5357b690d95bad39",
+    "url": "/static/js/2.79fc9184.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1d699064501a5d15306",
+    "url": "/static/js/main.a0e2233a.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
