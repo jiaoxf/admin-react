04d2a938e94b9e5377492ab5f0bb22eaba98ced5
焦学峰 <13261899915@163.com>
2020-03-31 10:08:46 +0800

    添加文件

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+class Home extends React.Component{
+  construcor(props){
+    super(props)
+
+  }
+  render(){
+    return(
+      <div>
+        <h1>这是Home页面</h1>
+      </div>
+    )
+  }
+}
+export default Home
---
+++ src/route/Router.js
@@ -0,0 +1,6 @@
+import React, { Component, Fragment } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+class App extends Component{
+
+}
